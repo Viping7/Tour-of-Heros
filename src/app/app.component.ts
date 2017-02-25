95ff65c6be242a6773e0b8beaab55eaf9badd2ac
Vipin Gopinath <vipin.vipin.g498@gmail.com>
2017-02-25 22:47:24 +0530

    Multiple Components
    Updated Till Multiple Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,18 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
+const HEROS: Hero[]= [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+{ id: 20, name: 'Tornado' }];
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +20,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hello';
+  title = 'Tour of Heros';
+  heros=HEROS;   
+  onSelect=function(hero){
+    this.selectedHero=hero;
+}    
 }
